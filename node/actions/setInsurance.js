b0c80c586accfe2b2d3436e25fe596becf1c63ae
JakubKotulski <jakub.kotulski@interia.com>
2023-04-14 23:10:49 +0200

    reset days

--- node/actions/setInsurance.js
+++ node/actions/setInsurance.js
@@ -2,7 +2,10 @@ const User = require("../models/User");
 
 const setInsurance = async (req, res) => {
   try {
-    await User.updateOne({ _id: req.user._id }, { $set: { insurance: false } });
+    await User.updateOne(
+      { _id: req.user._id },
+      { $set: { insurance: false, insuranceDay: req.body.insuranceDay, insuranceMonth: req.body.insuranceMonth } }
+    );
     return res.status(200).send("OK");
   } catch (err) {
     console.log(err);
--- node/actions/setTechnicalReview.js
+++ node/actions/setTechnicalReview.js
@@ -2,7 +2,16 @@ const User = require("../models/User");
 
 const setTechnicalReview = async (req, res) => {
   try {
-    await User.updateOne({ _id: req.user._id }, { $set: { technicalReview: false } });
+    await User.updateOne(
+      { _id: req.user._id },
+      {
+        $set: {
+          technicalReview: false,
+          technicalReviewDay: req.body.technicalReviewDay,
+          technicalReviewMonth: req.body.technicalReviewMonth,
+        },
+      }
+    );
     return res.status(200).send("OK");
   } catch (err) {
     console.log(err);
